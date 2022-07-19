2737d59cefa516700920e6927367ccf40d456a12
arpio <alex@familyrutz.com>
2022-07-18 21:33:54 -0600

    drivers: media: Replace missing includes from previous commit

--- drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
+++ drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
@@ -785,7 +785,7 @@ static int msm_vidc_pm_suspend(struct device *dev)
 
 static int msm_vidc_pm_resume(struct device *dev)
 {
-	update_marker("vidc resumed");
+	place_marker("vidc resumed");
 	dprintk(VIDC_INFO, "%s\n", __func__);
 	return 0;
 }
